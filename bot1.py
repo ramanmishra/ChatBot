38e2ab3dd75e8bef727ac821dd9ea97721c84f93
Raj Parekh <rajparekh1994@gmail.com>
2018-05-27 14:38:41 +0530

    correcting the path and termination condition

--- bot1.py
+++ bot1.py
@@ -6,15 +6,17 @@ from chatterbot.trainers import ListTrainer
 bot = ChatBot('Bot')
 bot.set_trainer(ListTrainer)
 
-for files in os.listdir('C:\makathon\ChatBot'):
-             data = open('C:\makathon\ChatBot\data\\'+files,'r').readlines()
+for files in os.listdir('C:/Users/Parth - Pc/Desktop/ChatBot/data'):
+             data = open('C:/Users/Parth - Pc/Desktop/ChatBot/data/'+ files,'r').readlines()
              bot.train(data)
 
 while True:
         message=input('You:')
+		
         if message.strip()!='Bye':
             reply = bot.get_response(message)
             print('Chatbot:',reply)
-        if message.strip()=='Bye':
+			
+        if message.strip().lower() =='bye':
             print('Chatbot:Bye')
             break
